fba16aadd0d4b06c00ffef7f92716d0621b39f01
Sanna Ishaq <sakinah.ishaq@gmail.com>
2022-04-26 08:44:15 -0400

    commit tree e4ef2ab024c4aa24bcba3c4227e4a4c0ee2ae278

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,11 @@
+class PlantsController < ApplicationController
+    def index
+        plants = Plant.all
+        render json: plants
+    end
+    def show
+        plant = Plant.find_by(id: params[:id])
+        render json: plant
+    end
+    
+end
